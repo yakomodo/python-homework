87e2cf873cefaa827c6d8d9b1f941b7f49cdb354
yakomodo <104077554+yakomodo@users.noreply.github.com>
2026-02-09 13:47:51 -0600

    is_palindrome

---
+++ is_palindrome_8_2.py
@@ -0,0 +1,10 @@
+import string
+def is_palindrome(text): 
+    cleaned = ''.join(char.lower() for char in text if char not in string.punctuation and char != ' ')
+    return cleaned == cleaned[::-1]
+    
+assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1' 
+assert is_palindrome('0P') == False, 'Test2' 
+assert is_palindrome('a.') == True, 'Test3' 
+assert is_palindrome('aurora') == False, 'Test4' 
+print("ОК")
